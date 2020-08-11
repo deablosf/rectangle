b1d459a3507b0991ab20d74cd7626fee3c84df29
Student_PSXGA136 <deablosf@hotmail.com>
2020-08-11 15:21:01 -0400

    done

--- src/org/perscholas/rec/Rectangle.java
+++ src/org/perscholas/rec/Rectangle.java
@@ -1,4 +1,27 @@
 package org.perscholas.rec;
 
 public class Rectangle {
+    double width = 0;
+    double length = 0;
+
+    public Rectangle (double width, double length) {
+        this.width = width;
+        this.length = length;
+
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+
+
 }
